851c5b1557c4365776bee635672a9e6e09722672
samidss56 <adimas.id@gmail.com>
2024-02-20 23:44:27 +0700

    api routes and firebase database connection

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,8 +1,16 @@
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 
+type productType = {
+  id: number;
+  name: string;
+  price: number;
+  size: string;
+};
+
 const ProductPage = () => {
-  const [isLogin, setIsLogin] = useState(false);
+  const [isLogin, setIsLogin] = useState(true);
+  const [products, setProducts] = useState([]);
   const { push } = useRouter();
 
   useEffect(() => {
@@ -11,9 +19,22 @@ const ProductPage = () => {
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  useEffect(() => {
+    fetch("/api/product")
+      .then((res) => res.json())
+      .then((response) => {
+        setProducts(response.data);
+      });
+  }, []);
+
   return (
-    <div>
-      <h1>Product Page</h1>
+    <div className="p-10">
+      <h1 className="text-3xl font-bold mb-2">Product Page</h1>
+      <h2 className="mb-2">Products :</h2>
+      {products.map((product: productType) => (
+        <li key={product.id}>{product.name}</li>
+      ))}
     </div>
   );
 };
