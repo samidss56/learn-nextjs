b358bf961f5e7fbc657bb0a19b4d8fb1eadbaaff
samidss56 <adimas.id@gmail.com>
2024-02-20 14:53:07 +0700

    nested routing, dynamic routing, atomic design concepts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
   return (
-    <div>Hello There</div>
-  )
+    <div>
+      <h1>Hello There</h1>
+    </div>
+  );
 }
